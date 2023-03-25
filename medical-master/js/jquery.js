403e37af245b8a9006a3142e8bd3189d630a40ab
dhruv-3000 <89414014+dhruv-3000@users.noreply.github.com>
2023-03-25 17:51:06 +0530

    Add files via upload

---
+++ medical-master/js/jquery.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+sr.reveal('.main-info', { duration: 1000 });
+sr.reveal('.contact', { duration: 1000 });
+sr.reveal('.offer', { duration: 1000 });
+sr.reveal('.appointment', { duration: 1000 });
+sr.reveal('.persons', { duration: 1000 });
+sr.reveal('.heart', { duration: 1000 });
+
+sr.reveal('.aboutus', { duration: 1000 });
+sr.reveal('.separator', { duration: 1000 });
+sr.reveal('.factors', { duration: 1000 });
+sr.reveal('.details .detail1', { duration: 1000 });
+sr.reveal('.details .detail2', { duration: 1000 });
+sr.reveal('.details .detail3', { duration: 1000 });
+
+sr.reveal('.offer1', { duration: 1000 });
+sr.reveal('.offer2', { duration: 1000 });
+sr.reveal('.offer3', { duration: 1000 });
+sr.reveal('.offer4', { duration: 1000 });
